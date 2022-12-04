5e8deb9cd6f9b64bdf754a802c70e68b02a2aea8
Alexander Savchenko <alexander.savchenko@djangostars.com>
2022-12-04 11:14:40 +0200

    Day 4 complete

---
+++ Advent2022-4-2.js
@@ -0,0 +1,26 @@
+import range from 'lodash/range'
+const input = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`
+
+
+const pairs = input.split('\n').map(pairs => pairs.split(','))
+
+let result = 0
+
+pairs.forEach(([first, second]) => {
+    let [firstStart, firstEnd] = first.split('-')
+    let [secondStart, secondEnd] = second.split('-')
+    const firstRange = range(firstStart, ++firstEnd)
+    const secondRange = range(secondStart, ++secondEnd)
+    if(firstRange.some(val => secondRange.includes(val))) {
+        result += 1
+    } else if(secondRange.some(val => firstRange.includes(val))) {
+        result += 1
+    }
+})
+
+console.log('Result: ', result) //? 4
